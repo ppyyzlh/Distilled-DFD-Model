2ef69a28d6e046828ada638f5fe46a018393cc06
pengyuan <1664667489@qq.com>
2023-05-22 16:21:05 +0800

    remove match

--- models/config.py
+++ models/config.py
@@ -3,10 +3,9 @@ from efficientnet_pytorch import EfficientNet
 from torch import nn
 
 def config_model(model_name, num_classes):
-    match model_name:
-        case "inceptionresnetv2":
+    if model_name == "inceptionresnetv2":
             model = inceptionresnetv2(num_classes) 
-        case "efficientnet-b7":
+    elif model_name == "efficientnet-b7":
             model = EfficientNet.from_name('efficientnet-b7')
             model._fc = nn.Linear(model._fc.in_features, num_classes)
     return model
