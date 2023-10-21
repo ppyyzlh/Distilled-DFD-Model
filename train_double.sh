94f39dad261b435a11d81ef46ec7a7c89624dc95
founSword <wuziqiu666@outlook.com>
2023-10-21 18:08:48 +0800

    add double sh

--- train_double.sh
+++ train_double.sh
@@ -1,9 +1,8 @@
 eval "$(conda shell.bash hook)"
 echo 'start train teacher'
-conda activate ppyy3.7
-python3 -u train-teacher.py >> train-teacher.log
+conda activate py3.10
+python3 -u train-teacher.py --config /home/pengyuan/Distilled-DFD-Model/configs/train_teacher.yaml >> train-teacher.log
 echo 'complete train teacher'
 echo 'start train student'
-python3 -u train-student.py >> train-student.log
+python3 -u train-student.py --config /home/pengyuan/Distilled-DFD-Model/configs/train_student.yaml >> train-student.log
 echo 'complete train student'
-
