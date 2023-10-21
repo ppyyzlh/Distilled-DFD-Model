2bd1866b610715b01b44548cfb57d1b1c6c122bc
founSword <wuziqiu666@outlook.com>
2023-10-21 18:44:35 +0800

    fix crop face

--- train_double.sh
+++ train_double.sh
@@ -1,8 +1,8 @@
 eval "$(conda shell.bash hook)"
 echo 'start train teacher'
 conda activate py3.10
-python3 -u train-teacher.py --config /home/pengyuan/Distilled-DFD-Model/configs/train_teacher.yaml >> train-teacher.log
+python3 -u train-teacher.py --config configs/train_teacher.yaml
 echo 'complete train teacher'
 echo 'start train student'
-python3 -u train-student.py --config /home/pengyuan/Distilled-DFD-Model/configs/train_student.yaml >> train-student.log
+python3 -u train-student.py --config configs/train_student.yaml
 echo 'complete train student'
