cfa76932c1ec219d6fcb96cafa7fcfe03e69cda6
wuziqiu <wuziqiu666@outlook.com>
2024-01-21 22:12:04 +0800

    Add Swin Transformer model and configuration

--- networks/config.py
+++ networks/config.py
@@ -1,5 +1,7 @@
 from .inceptionresnetv2 import inceptionresnetv2
 from efficientnet_pytorch import EfficientNet
+from swin_transformer.config import get_config
+from swin_transformer.models import build_model
 from torch import nn
 
 def config_model(model_name, num_classes):
@@ -8,5 +10,7 @@ def config_model(model_name, num_classes):
     elif model_name == "efficientnet-b7":
             model = EfficientNet.from_name('efficientnet-b7')
             model._fc = nn.Linear(model._fc.in_features, num_classes)
+    elif model_name == "swin_transformer":
+           model = build_model(get_config())
     return model
             
